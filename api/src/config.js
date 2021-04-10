0ae152a8c0dc053b706daf0ad5c86fbea606bbe4
Aarya Bhatia <aarya@Aaryas-MacBook-Pro.local>
2021-04-10 12:57:51 +0530

    fix

--- api/src/config.js
+++
@@ -1,6 +0,0 @@
-require("dotenv").config();
-
-module.exports.app_id = process.env.app_id;
-module.exports.app_key = process.env.app_key;
-module.exports.base_url = "https://api.edamam.com";
-module.exports.port = process.env.PORT || 4000;
