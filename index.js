c6742a4e8bc4dff7b31e191510292cddc1538104
Aarya Bhatia <aarya@Aaryas-MacBook-Pro.local>
2021-04-10 10:31:32 +0530

    setup project

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
+require("dotenv").config();
 
+const app_id = process.env.app_id;
+const app_key = process.env.app_key;
+
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+
+const base_url = "https://api.edamam.com";
+
+app.use(express.json());
+
+function loadRecipes(searchWord) {
+  fetch(getSearchURL(searchWord))
+    .then((res) => res.json())
+    .then((res) => {
+      console.log(res.hits[0].label);
+    })
+    .catch((err) => console.log(err));
+}
+
+function getSearchURL(q) {
+  return `${base_url}/search?q=${q}&app_id=${app_id}&app_key=${app_key}`;
+}
+
+app.get("/", (req, res) => {
+  res.json({ message: "Hello" });
+});
+
+app.listen(port, () => console.log("Server running on port: " + port));
