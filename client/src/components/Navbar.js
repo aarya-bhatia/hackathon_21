f9b68a46c1d5f8b4d06ffd0cf168c38738062377
Aarya Bhatia <aarya@Aaryas-MacBook-Pro.local>
2021-04-11 18:41:25 +0530

    made outline nav component

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -2,26 +2,28 @@ import React from "react";
 
 export default function Navbar(props) {
   return (
-    <div>
-      <nav className="teal">
-        <div className="nav-wrapper navbar container">
-          <ul id="nav-mobile" className="right hide-on-small-and-down">
-            <li>
-              <a href="!">
-                <span className="fa fa-home fa-2x" aria-hidden="true" />
-              </a>
-            </li>
-            <li>
-              <a className="navbar-action" href="!">
-                <span
-                  className="fa fa-bookmark fa-2x"
-                  aria-hidden="true"
-                ></span>
-              </a>
-            </li>
-          </ul>
+    <div className="nav-container">
+      <div className="nav-left-panel">
+        <div className="nav-logo-container">
+          <img src="" alt="Logo" />
         </div>
-      </nav>
+      </div>
+
+      <div className="nav-middle-panel">
+        <div className="nav-middle-content-container">
+          <div className="nav-search-container">
+            <input type="text" className="nav-search-input" />
+          </div>
+
+          <div className="nav-quick-search-container">
+            <div className="nav-quick-search-item">Vegetarian</div>
+            <div className="nav-quick-search-item">Vegan</div>
+            <div className="nav-quick-search-item">Nonvegetarian</div>
+          </div>
+        </div>
+      </div>
+
+      <div className="nav-right-panel"></div>
     </div>
   );
 }
