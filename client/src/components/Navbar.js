c9e2c7fd3618de841615477a8b096134a99eac58
Aarya Bhatia <aarya@Aaryas-MacBook-Pro.local>
2021-04-11 18:58:25 +0530

    added parts to the nav

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -3,27 +3,55 @@ import React from "react";
 export default function Navbar(props) {
   return (
     <div className="nav-container">
-      <div className="nav-left-panel">
-        <div className="nav-logo-container">
-          <img src="" alt="Logo" />
+      <div className="left-panel">
+        <div className="logo-container">
+          <img src="assets/logo.png" alt="Logo" />
         </div>
       </div>
 
-      <div className="nav-middle-panel">
-        <div className="nav-middle-content-container">
-          <div className="nav-search-container">
-            <input type="text" className="nav-search-input" />
+      <div className="middle-panel">
+        <div className="middle-content-container">
+          <div className="search-container">
+            <span className="material-icons"></span>
+            <input
+              type="text"
+              className="search-input"
+              placeholder="Search recipes with..."
+            />
           </div>
 
-          <div className="nav-quick-search-container">
-            <div className="nav-quick-search-item">Vegetarian</div>
-            <div className="nav-quick-search-item">Vegan</div>
-            <div className="nav-quick-search-item">Nonvegetarian</div>
+          <div className="quick-search-container">
+            <div className="quick-search-item">
+              <div className="chip">Veg</div>
+            </div>
+            <div className="quick-search-item">
+              <div className="chip">Non Veg</div>
+            </div>
+            <div className="quick-search-item">
+              <div className="chip">Non Dairy</div>
+            </div>
           </div>
         </div>
       </div>
 
-      <div className="nav-right-panel"></div>
+      <div className="nav-right-panel">
+        <div className="row">
+          <div className="col s6">
+            <span class="material-icons">person_outline</span>
+          </div>
+          <div className="col s6">
+            <span class="material-icons">menu</span>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col s6">
+            <span class="material-icons">explore</span>
+          </div>
+          <div className="col s6">
+            <span class="material-icons">favorite</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
