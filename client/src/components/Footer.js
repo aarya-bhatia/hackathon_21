628128517b7cfbedfc36b921d1abf535a5cf8d9f
Aarya Bhatia <aarya@Aaryas-MacBook-Pro.local>
2021-04-11 19:15:54 +0530

    footer design and css

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -2,31 +2,14 @@ import React from "react";
 
 export default function Footer(props) {
   return (
-    <footer className="page-footer green">
-      <div className="row">
-        <div
-          className=""
-          style={{
-            padding: 20,
-          }}
-        >
-          <div className="col">
-            <a href="!">
-              <h5 className="white-text">About Us</h5>
-            </a>
-          </div>
-          <div className="col">
-            <a href="!">
-              <h5 className="white-text">Help</h5>
-            </a>
-          </div>
-          <div className="col">
-            <a href="!">
-              <h5 className="white-text">Contact</h5>
-            </a>
-          </div>
-        </div>
+    <div className="footer yellow darken-2">
+      <div className="footer-container">
+        <div className="footer-item">About Us</div>
+        <div className="footer-item">|</div>
+        <div className="footer-item">Help</div>
+        <div className="footer-item">|</div>
+        <div className="footer-item">Contact Us</div>
       </div>
-    </footer>
+    </div>
   );
 }
